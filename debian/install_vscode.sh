b945bc70ead6019c663b4286da4402dd594eabc9
Celestine Dias <celestinediask@gmail.com>
2024-07-03 20:12:06 +0530

    edited install_vscode.sh

--- debian/install_vscode.sh
+++ debian/install_vscode.sh
@@ -1,5 +1,23 @@
 # shell script to install vscode in debian system
 
-wget -P /tmp https://code.visualstudio.com/sha/download?build=insider&os=linux-deb-x64
+set -e
 
-sudo apt install /tmp/code-insiders_*_amd64.deb
+# Check if OS is Debian-based
+if ! grep -q "^ID=debian" /etc/os-release; then
+    echo "This script is intended for Debian-based distributions only. Exiting."
+    exit 1
+fi
+
+# Check if running with root privilage
+if [ "$(id -u)" != "0" ]; then
+    if ! sudo -n true 2>/dev/null; then
+        echo "This script requires root privileges to execute."
+    fi
+    
+    sudo sh "$0" "$@"
+    exit $?
+fi
+
+wget -P /tmp/code-insiders.deb "https://code.visualstudio.com/sha/download?build=insider&os=linux-deb-x64"
+
+sudo apt install /tmp/code-insiders.deb
