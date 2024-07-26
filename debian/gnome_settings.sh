22fdf25def382e3de647c2ce4b29354b7af3abb6
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-07-26 06:09:11 -0400

    Update gnome_settings.sh
    disable recent history

--- debian/gnome_settings.sh
+++ debian/gnome_settings.sh
@@ -4,6 +4,7 @@ gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
 gsettings set org.gnome.desktop.interface gtk-theme 'HighContrastInverse'
 gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
 
+gsettings set org.gnome.desktop.privacy remember-recent-files false
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop', 'firefox.desktop', 'google-chrome.desktop']"
