eb6286630c3d87685d7de535173d84945111e4e1
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-13 14:12:44 +0530

    added few comments

--- debian/system.sh
+++ debian/system.sh
@@ -1,5 +1,6 @@
 # Check if OS is Debian-based
 check_os() {
+	echo "checking if OS is debian based..."
 	if ! grep -q "^ID=debian" /etc/os-release; then
 		echo "This script is intended for Debian-based distributions only. Exiting."
 		exit 1
@@ -8,13 +9,15 @@ check_os() {
 
 # Check if running with root privilage
 check_root() {
+	echo "checking root privilage..."
 	if [ "$EUID" -ne 0 ]; then
-    echo "Error: This script must be run as root!" >&2
-    exit 1
-  fi
+		echo "Error: This script must be run as root!" >&2
+		exit 1
+	fi
 }
 
 comment_out_deb_src() {
+	echo "running: commenting out deb_src..."
     # Check if backup file already exists
     if grep -qE '^\s*#.*deb-src' /etc/apt/sources.list; then
         echo "Already deb-src lines have been commented out in /etc/apt/sources.list."
@@ -31,11 +34,13 @@ comment_out_deb_src() {
 }
 
 update_system() {
+	echo "updating and upgrading system..."
 	sudo apt update
 	sudo apt upgrade
 }
 
 fix_wifi() {
+	echo "fixing wifi..."
 	sudo mv -i /etc/network/interfaces ~/interfaces.bak
 	sudo systemctl restart wpa_supplicant.service
 	sudo systemctl restart NetworkManager
