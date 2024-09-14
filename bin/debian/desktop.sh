ddfb9927deca4f63cec0e96ec514e6604028f47c
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-14 10:30:11 +0530

    Update desktop.sh
    google-chrome-stable package not found fix

--- bin/debian/desktop.sh
+++ bin/debian/desktop.sh
@@ -13,16 +13,12 @@ source $project_root/swap.sh
 source $project_root/debian/apps/chrome.sh
 
 check_os
-
 comment_out_deb_src
+add_repo_chrome
+sudo apt update
 
 sudo apt install gnome-session --no-install-recommends --no-install-suggests gdm3 kitty vim -y
 sudo apt install mpv eog evince gnome-text-editor gnome-disk-utility gnome-system-monitor gnome-control-center fonts-mlym nautilus firefox-esr curl dbus-x11 wpasupplicant network-manager google-chrome-stable -y
-
-
-add_repo_chrome
-
-sudo apt update
 
 fix_wifi
 
