4d4f58ee6286dc5266b23a96312cc13ac5780941
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-15 21:16:53 +0530

    path fix

--- bin/debian/desktop.sh
+++ bin/debian/desktop.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 # debian gnome desktop setup
 
 set -e
 
 sudo test || true
 
-project_root="../.."
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-source $project_root/debian/system.sh 
-source $project_root/gnome.sh 
-source $project_root/swap.sh
-source $project_root/debian/apps/chrome.sh
+PROJECT_ROOT="../.."
+
+source $PROJECT_ROOT/debian/system.sh 
+source $PROJECT_ROOT/gnome.sh 
+source $PROJECT_ROOT/swap.sh
+source $PROJECT_ROOT/debian/apps/chrome.sh
 
 check_os
 comment_out_deb_src
@@ -27,10 +29,14 @@ gnome_settings_host
 set_swap
 
 # setup firefox profile
-git clone https://github.com/celestinediask/firefox $project_root/tmp/firefox
-$project_root/tmp/firefox/remove_bloat.sh
+cd $PROJECT_ROOT/tmp
+git clone https://github.com/celestinediask/firefox
+./remove_bloat.sh
 
 # import config
-cp -i $project_root/config/vimrc ~/.vimrc
+cd $PROJECT_ROOT/config
+cp -i vimrc ~/.vimrc
+
+cd $SCRIPT_DIR
 
 echo "All done!"
