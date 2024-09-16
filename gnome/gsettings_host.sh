c8f6f985029327f887fac88e7e33bd25b9c62619
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-16 16:33:34 +0530

    Create gsettings_host.sh

---
+++ gnome/gsettings_host.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# gnome gsettings for host machine
+
+set -e
+
+echo "setting gnome settings for host machine..."
+# set dark theme
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'HighContrastInverse' # for legacy apps
+
+# set icon theme
+gsettings set org.gnome.desktop.interface icon-theme 'Adwaita'
+  
+# disable file history
+gsettings set org.gnome.desktop.privacy remember-recent-files false
+
+# enable tap to click
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+
+# show battery percentage
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+# set favorite apps
+gsettings set org.gnome.shell favorite-apps "['kitty.desktop', 'org.gnome.Nautilus.desktop', 'firefox.desktop', 'google-chrome.desktop']"
+
+# set background color
+gsettings set org.gnome.desktop.background primary-color '#000000'
+
+# enable over amplification
+gsettings set org.gnome.desktop.sound allow-volume-above-100-percent 'true'
+
+echo "custom gnome settings applied"
