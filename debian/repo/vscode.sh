2a12dd468ab38e3f682cb04c8c8ff58cd4c16259
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-17 11:10:33 +0530

    Update vscode.sh

--- debian/repo/vscode.sh
+++ debian/repo/vscode.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # add vscode repo for debian
 
 set -e
@@ -13,12 +13,14 @@ if ! grep -q "^ID=debian" /etc/os-release; then
 	exit 1
 fi
 
-echo "Adding vscode repo..."
-sudo apt install -y wget gpg #gnupg
-# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/keyrings/microsoft-archive-keyring.gpg
-sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt install apt-transport-https
-
-echo "vscode repo added"
+add_vscode_repo() {
+	echo "Adding vscode repo..."
+	sudo apt install -y wget gpg #gnupg
+	# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+	wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+	sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/keyrings/microsoft-archive-keyring.gpg
+	sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+	sudo apt install apt-transport-https
+	
+	echo "vscode repo added"
+ }
