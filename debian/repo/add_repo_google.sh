5d688777933223bd41c73dda030a592df6d2d486
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-19 17:24:56 +0530

    Update and rename add_chrome_repo.sh to add_repo_google.sh

--- debian/repo/add_repo_google.sh
+++ debian/repo/add_repo_google.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "adding google repo to debian..."
+
 # Check if the system is Debian-based
 if ! grep -q "^ID=debian" /etc/os-release; then
 	echo "$THIS_SCRIPT is intended for Debian-based distributions only. Exiting."
@@ -25,4 +27,4 @@ sudo test || true
 echo "Adding google-chrome repo..."
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-echo "chrome repo added"
+echo "google repo added"
