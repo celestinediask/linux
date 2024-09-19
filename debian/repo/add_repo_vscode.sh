c2af195a6f3c4f1b050a1122ef31474cb0a40d86
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-19 17:21:19 +0530

    Update add_repo_vscode.sh

--- debian/repo/add_repo_vscode.sh
+++ debian/repo/add_repo_vscode.sh
@@ -34,4 +34,6 @@ sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/keyrings/microsoft-ar
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt install apt-transport-https
 
+rm microsoft.gpg
+
 echo "vscode repo added"
