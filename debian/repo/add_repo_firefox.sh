8b5b195182609540836f6efdb7642c69637aef5d
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-19 17:16:14 +0530

    Update and rename firefox.sh to add_repo_firefox.sh

---
+++ debian/repo/add_repo_firefox.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+sudo test || true
+
+
+echo "Adding firefox to debian repo..."
+
+# Check if the system is Debian-based
+if ! grep -q "^ID=debian" /etc/os-release; then
+	echo "$THIS_SCRIPT is intended for Debian-based distributions only. Exiting."
+	exit 1
+fi
+
+# Check if wget is installed
+if ! command -v wget &> /dev/null; then
+    echo "Error: wget is not installed."
+    exit 1
+fi
+
+# Check for internet connectivity
+if ! ping -c 1 debian.org > /dev/null 2>&1; then
+    echo "No internet connection. Exiting..."
+    exit 1
+fi
+
+# Create directory for APT repository keys if it doesn't exist
+sudo install -d -m 0755 /etc/apt/keyrings
+
+# Import Mozilla APT repository signing key
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+# Initialize GPG keyring
+gpg --list-keys
+
+# Verify the key fingerprint
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | \
+	awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+
+# Add Mozilla APT repository to sources list
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+
+# Configure APT to prioritize packages from the Mozilla repository
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+
+echo "firefox repo added to debian repo"
