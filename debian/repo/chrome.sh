31ca6979a7bd3fc8ff2a2feb9e2cbd4f1d32ec79
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-19 17:07:23 +0530

    Update chrome.sh

--- debian/repo/chrome.sh
+++ debian/repo/chrome.sh
@@ -2,18 +2,27 @@
 
 set -e
 
-add_repo_chrome() {
-	echo "Adding google-chrome repo"
-	wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo tee /etc/apt/trusted.gpg.d/google.asc >/dev/null
-	sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-	echo "chrome repo added"
-}
+echo "installing google-chrome directly..."
 
-install_chrome_manual() {
-	wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-	sudo apt install -y /tmp/google-chrome-stable_current_amd64.deb
-	sudo rm /tmp/google-chrome-stable_current_amd64.deb
-	echo "chrome installed"
-}
+# Check if the system is Debian-based
+if ! grep -q "^ID=debian" /etc/os-release; then
+	echo "$THIS_SCRIPT is intended for Debian-based distributions only. Exiting."
+	exit 1
+fi
 
-add_repo_chrome
+# Check if wget is installed
+if ! command -v wget &> /dev/null; then
+    echo "Error: wget is not installed."
+    exit 1
+fi
+
+# Check for internet connectivity
+if ! ping -c 1 debian.org > /dev/null 2>&1; then
+    echo "No internet connection. Exiting..."
+    exit 1
+fi
+
+wget -P /tmp https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install -y /tmp/google-chrome-stable_current_amd64.deb
+sudo rm /tmp/google-chrome-stable_current_amd64.deb
+echo "chrome installed"
