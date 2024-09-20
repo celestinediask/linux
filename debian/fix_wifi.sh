50bda2366e506c6b240cbd5da60c8b923784ace1
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-20 10:01:26 +0530

    Create fix_wifi.sh

---
+++ debian/fix_wifi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+THIS_SCRIPT=$(basename "$0")
+
+echo "running: $THIS_SCRIPT"
+echo "fixing wifi..."
+
+# Check if the system is Debian-based
+if ! grep -q "^ID=debian" /etc/os-release; then
+	echo "$THIS_SCRIPT is intended for Debian-based distributions only. Exiting."
+	exit 1
+fi
+
+# Check if the /etc/network/interfaces file exists
+if [ ! -f /etc/network/interfaces ]; then
+    echo "/etc/network/interfaces does not exist. skipping wifi fix."
+    exit 1
+fi
+
+sudo mv -i /etc/network/interfaces ~/interfaces.bak
+sudo systemctl restart wpa_supplicant.service
+sudo systemctl restart NetworkManager
+
+echo "successfully fixed wifi"
