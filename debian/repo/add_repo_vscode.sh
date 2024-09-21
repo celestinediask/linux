08813f0961aacbe124b30cc2ce1f74d8ccb6c7ca
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-21 09:39:30 +0530

    Update add_repo_vscode.sh

--- debian/repo/add_repo_vscode.sh
+++ debian/repo/add_repo_vscode.sh
@@ -15,16 +15,10 @@ if ! grep -q "^ID=debian" /etc/os-release; then
 	exit 1
 fi
 
-# Check if VSCode is installed
-if command -v code > /dev/null 2>&1; then
-  echo "VSCode is already installed. Exiting script."
-  exit 0
-fi
-
-# Check if wget is installed
-if ! command -v wget &> /dev/null; then
-    echo "Error: wget is not installed."
-    exit 1
+# Check if VSCode repo is already added
+if [ -f /etc/apt/sources.list.d/vscode.list ]; then
+    echo "VSCode repository is already added. Exiting script."
+    exit 0
 fi
 
 # Check for internet connectivity
@@ -33,13 +27,15 @@ if ! ping -c 1 debian.org > /dev/null 2>&1; then
     exit 1
 fi
 
-sudo apt install -y wget gpg #gnupg
+# install dependencies
+sudo apt install gpg wget apt-transport-https
+
 # curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/keyrings/microsoft-archive-keyring.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/microsoft-archive-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt install apt-transport-https
 
+# clean up
 rm microsoft.gpg
 
 echo "vscode repo added"
