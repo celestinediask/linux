bb5b5f3b88980ab7d09669ad06852b60faa169f9
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-09-21 07:54:25 +0530

    Update go-dev-env.sh

--- bin/debian/go-dev-env.sh
+++ bin/debian/go-dev-env.sh
@@ -1,7 +1,32 @@
+set -e
 # debian go dev env setup
 
-# codium
+sudo test || true
+
+PROJECT_ROOT="../.."
+
+# Check if the system is Debian-based
+if ! grep -q "^ID=debian" /etc/os-release; then
+	echo "$THIS_SCRIPT is intended for Debian-based distributions only. Exiting."
+	exit 1
+fi
+
+# Check for internet connectivity
+if ! ping -c 1 9.9.9.9 > /dev/null 2>&1; then
+    echo "No internet connection. Exiting..."
+    exit 1
+fi
+
 # postgres
-# go
-# docker
+sudo apt install postgresql
+
+# install vscode
+$PROJECT_ROOT/debian/repo/add_repo_vscode.sh
+
+# install go
+$PROJECT_ROOT/install_go.sh
+
 # postman
+$PROJECT_ROOT/install_postman.sh
+
+# install docker
