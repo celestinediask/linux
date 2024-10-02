4eabd8d305a473451e913ecb20a5bcdfb795fe9c
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-10-02 23:26:42 +0530

    Update install_vscode_test.sh

--- debian/repo/install_vscode_test.sh
+++ debian/repo/install_vscode_test.sh
@@ -4,6 +4,8 @@ set -e
 
 URL="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 
+sudo test || true
+
 echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
 
 wget $URL -O vscode.deb
