431238e7ef33efea3e0289d2afa5e71eb70971ec
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-10-02 18:19:04 +0530

    Update gsettings_host.sh
    pinned gnome-terminal to dash

--- gnome/gsettings_host.sh
+++ gnome/gsettings_host.sh
@@ -26,7 +26,7 @@ gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
 gsettings set org.gnome.desktop.interface show-battery-percentage true
 
 # set favorite apps
-gsettings set org.gnome.shell favorite-apps "['kitty.desktop', 'org.gnome.Nautilus.desktop', 'firefox-esr.desktop', 'firefox.desktop', 'google-chrome.desktop']"
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop', 'kitty.desktop', 'org.gnome.Nautilus.desktop', 'firefox-esr.desktop', 'firefox.desktop', 'google-chrome.desktop']"
 
 # set background color
 #gsettings set org.gnome.desktop.background primary-color '#023c88' # blue
