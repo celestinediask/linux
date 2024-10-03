81484ee6b386a85ef11e2cb6316209fc17d9652c
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-10-03 13:15:27 +0530

    added some gnome custom setttings

--- debian/debian_gnome_minimal.sh
+++ debian/debian_gnome_minimal.sh
@@ -11,10 +11,24 @@ start_time=$(date +%s)
 
 sudo apt update
 
-sudo apt install -y gnome-session gdm3 gnome-terminal
+sudo apt install -y gnome-session --no-install-suggests --no-install-recommends
+
+sudo apt install gdm3 gnome-terminal
+
+# switch legacy apps to dark them like gnome-terminal
+gsettings set org.gnome.desktop.interface gtk-theme 'HighContrastInverse'
 
 # pin gnome terminal to gnome dash
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop']"
+
+# enable tap to click
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+
+# show battery percentage
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+# enable over amplification
+gsettings set org.gnome.desktop.sound allow-volume-above-100-percent 'true'
 
 end_time=$(date +%s)
 
