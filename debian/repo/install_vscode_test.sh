0215122324f972d1868bc7a57dc3d572f3d6a29f
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-10-03 09:34:23 +0530

    Update install_vscode_test.sh
    execution time show

--- debian/repo/install_vscode_test.sh
+++ debian/repo/install_vscode_test.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+start_time=$(date +%s)
+
 URL="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
 
 sudo test || true
@@ -13,5 +15,9 @@ wget $URL -O vscode.deb
 sudo apt install ./vscode.deb
 
 rm vscode.deb
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+echo "completed in $execution_time seconds"
 
 echo "vscode has been successfully installed."
