d01dd8d6c6d38a4813c7de95e14727c09fdebe18
celestinediask <152255839+celestinediask@users.noreply.github.com>
2024-11-23 10:35:24 +0530

    Update guest_desktop.sh

--- debian/guest_desktop.sh
+++ debian/guest_desktop.sh
@@ -1,10 +1,31 @@
 #!/bin/bash
 # this script intended for virtual machine desktop setup
 
-# gnome-session core
-# gdm3
-# kitty
-# git
-# set git config
+set -e
+
+sudo test || true
+
+start_time=$(date +%s)
+
+./comment_deb_src.sh
+
+sudo apt update
+sudo apt install -y gnome-session --no-install-suggests --no-install-recommends
+sudo apt install -y gdm3 gnome-terminal spice-vdagebt
+
+./gnome_terminal_enable_bright_colors.sh
+
+# switch legacy apps to dark them like gnome-terminal
+gsettings set org.gnome.desktop.interface gtk-theme 'HighContrastInverse'
+
+# pin gnome terminal to gnome dash
+gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop']"
+
+# enable over amplification
+gsettings set org.gnome.desktop.sound allow-volume-above-100-percent 'true'
+
 # auto login
-# spice agent
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+echo "debian gnome minimal virtual machine setup has been successfully completed in $execution_time seconds."
