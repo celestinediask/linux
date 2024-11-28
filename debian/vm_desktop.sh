e0feff060b206df795c44d35d7f766f9f4bb89dc
etosy <152255839+etosy@users.noreply.github.com>
2024-11-28 10:48:18 +0530

    Update vm_desktop.sh
    corrected spelling mistake

--- debian/vm_desktop.sh
+++ debian/vm_desktop.sh
@@ -11,7 +11,7 @@ start_time=$(date +%s)
 
 sudo apt update
 sudo apt install -y gnome-session --no-install-suggests --no-install-recommends
-sudo apt install -y gdm3 gnome-terminal spice-vdagebt
+sudo apt install -y gdm3 gnome-terminal spice-vdagent
 
 ./gnome_terminal_enable_bright_colors.sh
 
