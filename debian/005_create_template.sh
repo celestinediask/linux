7a64b0013fd6d65e506d829365ebcd1dba979b86
etosy <152255839+etosy@users.noreply.github.com>
2024-12-06 01:57:48 +0530

    Update 005_create_template.sh

--- debian/005_create_template.sh
+++ debian/005_create_template.sh
@@ -1,8 +1,6 @@
 # create new txt file in templates
-if [ -d ~/Templates ]; then
-    # Create the new file
-    touch ~/Templates/new.txt
-    echo "File ~/Templates/new.txt created successfully."
-else
-    echo "Directory ~/Templates does not exist. Could not able to create new.txt template"
-fi
+
+mkdir ~/Templates
+touch ~/Templates/new.txt
+
+echo "File ~/Templates/new.txt created successfully."
