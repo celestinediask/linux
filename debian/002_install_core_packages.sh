53b2523748b15ed03451ca00ab369119ab93747c
etosy <152255839+etosy@users.noreply.github.com>
2024-12-06 01:01:07 +0530

    Update and rename 002_install_minimal_packages.sh to 002_install_core_packages.sh

--- debian/002_install_core_packages.sh
+++ debian/002_install_core_packages.sh
@@ -2,8 +2,14 @@
 
 set -e
 
+start_time=$(date +%s)
+
 sudo apt update
 
 sudo apt install -y gnome-session --no-install-suggests --no-install-recommends
 
 sudo apt install -y gdm3 gnome-terminal network-manager gnome-keyring wpasupplicant
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+echo "successfully installed core packages for debian in $execution_time seconds."
