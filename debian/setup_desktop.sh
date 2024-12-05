c93411b603a0a21a6487da0bc2b06bf5e59672a4
etosy <152255839+etosy@users.noreply.github.com>
2024-12-06 01:23:16 +0530

    Update setup_desktop.sh

--- debian/setup_desktop.sh
+++ debian/setup_desktop.sh
@@ -6,19 +6,10 @@ sudo test || true
 
 start_time=$(date +%s)
 
-./001_comment_deb_src.sh
-./002_install_minimal_packages.sh
-./gnome_terminal_enable_bright_colors.sh
-./install_essential_packages.sh
-../gnome/gsettings_host.sh
-
-# install chrome
-
-# pin gnome terminal to gnome dash
-gsettings set org.gnome.shell favorite-apps "['org.gnome.Terminal.desktop']"
-
-# this will disconnect the internet until reboot therefore this line should be placed after all the internet required tasks
-./fix_wifi
+for script in $(ls [0-9][0-9][0-9]_filename* | sort); do
+    echo "Executing $script"
+    #./"$script"
+done
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
