4880773e642fb4b7841eca9286f14ef5a2d34207
etosy <152255839+etosy@users.noreply.github.com>
2024-12-06 20:45:41 +0530

    Update vm_desktop.sh

--- debian/vm_desktop.sh
+++ debian/vm_desktop.sh
@@ -7,19 +7,21 @@ sudo test || true
 
 start_time=$(date +%s)
 
-bash 001_comment_deb_src.sh 2>&1 | tee -a logfile.log
+exec > >(tee -a "logfile.log") 2>&1
 
-sudo apt update 2>&1 | tee -a logfile.log
-sudo apt install -y gnome-session --no-install-suggests --no-install-recommends 2>&1 | tee -a logfile.log
-sudo apt install -y gdm3 gnome-terminal spice-vdagent 2>&1 | tee -a logfile.log
+bash 001_comment_deb_src.sh
 
-bash 003_gnome_terminal_enable_bright_colors.sh 2>&1 | tee -a logfile.log
+sudo apt update
+sudo apt install -y gnome-session --no-install-suggests --no-install-recommends
+sudo apt install -y gdm3 gnome-terminal spice-vdagent
 
-bash ../gdm3_enable_autologin.sh 2>&1 | tee -a logfile.log
+bash 003_gnome_terminal_enable_bright_colors.sh
 
-bash ../gnome/gsettings_host.sh 2>&1 | tee -a logfile.log
-bash ../gnome/gsettings_guest.sh 2>&1 | tee -a logfile.log
+bash ../gdm3_enable_autologin.sh
+
+bash ../gnome/gsettings_host.sh
+bash ../gnome/gsettings_guest.sh
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
-echo "debian gnome minimal virtual machine setup has been successfully completed in $execution_time seconds." 2>&1 | tee -a logfile.log
+echo "debian gnome minimal virtual machine setup has been successfully completed in $execution_time seconds."
