73a97aed26df1e465595e3820a26e74a91b543c0
etosy <152255839+etosy@users.noreply.github.com>
2024-12-06 19:35:48 +0530

    Update vm_desktop.sh

--- debian/vm_desktop.sh
+++ debian/vm_desktop.sh
@@ -7,18 +7,18 @@ sudo test || true
 
 start_time=$(date +%s)
 
-./001_comment_deb_src.sh
+bash 001_comment_deb_src.sh
 
 sudo apt update
 sudo apt install -y gnome-session --no-install-suggests --no-install-recommends
 sudo apt install -y gdm3 gnome-terminal spice-vdagent
 
-./003_gnome_terminal_enable_bright_colors.sh
+bash 003_gnome_terminal_enable_bright_colors.sh
 
-../gdm3_disable_autologin.sh
+bash ../gdm3_disable_autologin.sh
 
-../gnome/gsettings_host.sh
-../gnome/gsettings_guest.sh
+bash ../gnome/gsettings_host.sh
+bash ../gnome/gsettings_guest.sh
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
