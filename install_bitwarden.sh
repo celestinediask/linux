5cb4af88b14d447afd60dc609ffc5c384427afc6
etosy <152255839+etosy@users.noreply.github.com>
2024-12-06 14:25:24 +0530

    Update install_bitwarden.sh

--- install_bitwarden.sh
+++ install_bitwarden.sh
@@ -5,7 +5,7 @@ set -e
 sudo mkdir /opt/bitwarden
 
 sudo wget 'https://vault.bitwarden.com/download/?app=desktop&platform=linux' -O /opt/bitwarden/bitwarden.AppImage
-sudo wget 'https://raw.githubusercontent.com/bitwarden/brand/e755957e1ae5b84521a4a2491b791e743936af1a/icons/128x128.png' -O /opt/bitwarden/bitwarden.png
+sudo wget 'https://raw.githubusercontent.com/etosy/icons/refs/heads/main/bitwarden.png' -O /opt/bitwarden/bitwarden.png
 
 cat << EOF | sudo tee /usr/share/applications/bitwarden.desktop
 [Desktop Entry]
