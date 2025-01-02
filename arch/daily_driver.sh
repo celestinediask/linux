b516f481caf1f4af43f9a10cfd0d695e060d8849
etosy <152255839+etosy@users.noreply.github.com>
2025-01-02 09:03:57 +0530

    no confirmation for installing pkgs

--- arch/daily_driver.sh
+++ arch/daily_driver.sh
@@ -4,5 +4,5 @@ cp bashrc ~/.bashrc
 
 sudo pacman -Syu
 sudo pacman -S --needed vim nano bash-completion tree
-sudo pacman -S --needed gnome-session gdm gnome-console
+sudo pacman -S --needed --noconfirm gnome-session gdm gnome-console
 sudo systemctl enable gdm
