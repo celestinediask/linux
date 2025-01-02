aa0ece66315d2a1cd22ff870941e47ebaab46c02
etosy <152255839+etosy@users.noreply.github.com>
2025-01-02 08:59:05 +0530

    only install if not installed already

--- arch/daily_driver.sh
+++ arch/daily_driver.sh
@@ -1,6 +1,6 @@
 # arch daily driver
 
 cp bashrc ~/.bashrc
-sudo pacman -S vim nano bash-completion tree
-sudo pacman -S gnome-session gdm gnome-console
+sudo pacman -S --needed vim nano bash-completion tree
+sudo pacman -S --needed gnome-session gdm gnome-console
 sudo systemctl enable gdm
