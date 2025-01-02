1b65ca577e5f561ada9156cf327d32a5578f9af3
etosy <152255839+etosy@users.noreply.github.com>
2025-01-02 08:59:51 +0530

    update system first before installing pkgs

--- arch/daily_driver.sh
+++ arch/daily_driver.sh
@@ -1,6 +1,8 @@
 # arch daily driver
 
 cp bashrc ~/.bashrc
+
+sudo pacman -Syu
 sudo pacman -S --needed vim nano bash-completion tree
 sudo pacman -S --needed gnome-session gdm gnome-console
 sudo systemctl enable gdm
