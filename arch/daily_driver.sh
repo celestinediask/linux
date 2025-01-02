cb479814e48d1cc6bb7683755c57d2f145490ed7
etosy <152255839+etosy@users.noreply.github.com>
2025-01-02 09:05:21 +0530

    noconfirm for pkg installation

--- arch/daily_driver.sh
+++ arch/daily_driver.sh
@@ -3,6 +3,6 @@
 cp bashrc ~/.bashrc
 
 sudo pacman -Syu
-sudo pacman -S --needed vim nano bash-completion tree
+sudo pacman -S --needed --noconfirm vim nano bash-completion tree
 sudo pacman -S --needed --noconfirm gnome-session gdm gnome-console
 sudo systemctl enable gdm
