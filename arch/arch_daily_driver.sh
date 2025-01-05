ca6e217f2f728509b81d04447ab5e5abad6fc3ef
etosy <152255839+etosy@users.noreply.github.com>
2025-01-05 16:00:34 +0530

    Update arch_daily_driver.sh

--- arch/arch_daily_driver.sh
+++ arch/arch_daily_driver.sh
@@ -13,11 +13,24 @@ sudo pacman -S --needed --noconfirm - < ../packages.txt
 # gnome setup
 sudo pacman -S --needed --noconfirm - < ../gnome/core_packages.txt
 sudo pacman -S --needed --noconfirm - < ../gnome/essential_packages.txt
+# gsettings
 
 sudo systemctl enable gdm
 
+# setup yay
+
+# setup bluetooth
+sudo pacman -S bluez bluez-utils
+sudo systemctl start bluetooth
+sudo systemctl enable bluetooth
+
 # setup browser
-sudo pacman -S firefox chromium
+sudo pacman -S --noconfirm firefox
+# setup firefox profile
+sudo pacman -S firefox-ublock-origin
+sudo pacman -S firefox-dark-reader
+
+sudo pacman -S --noconfirm chromium
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
