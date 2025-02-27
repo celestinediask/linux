651deeafec5d5c443fb01aebd8a9bee33db52760
etosy <152255839+etosy@users.noreply.github.com>
2025-02-28 04:38:57 +0530

    Update setup_debian_netinst_gnome_desktop.sh

--- setup_debian_netinst_gnome_desktop.sh
+++ setup_debian_netinst_gnome_desktop.sh
@@ -6,12 +6,13 @@ sudo test || true
 
 start_time=$(date +%s)
 
-for script in $(ls [0-9][0-9][0-9]_* | sort); do
-    echo "================================================================================================"
-    echo "Executing $script"
-    echo "================================================================================================"
-    bash "$script"
-done
+bash debian/clean_repo.sh
+bash debian/install_core_packages.sh
+bash debian/install_essential_packages.sh
+bash setup_firefox_profile.sh
+bash gnome/gnome_terminal_enable_bright_colors.sh
+bash gnome/gsettings_host.sh
+bash debian/fix_wifi.sh
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
