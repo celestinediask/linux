30b04168e02074d5364d9c059ee222de8f16a2d3
etosy <152255839+etosy@users.noreply.github.com>
2025-02-28 05:19:35 +0530

    Update install_core_packages.sh

--- debian/install_core_packages.sh
+++ debian/install_core_packages.sh
@@ -7,8 +7,8 @@ start_time=$(date +%s)
 sudo apt update
 
 sudo apt install -y gnome-session --no-install-suggests --no-install-recommends
-
-sudo apt install -y gdm3 gnome-terminal network-manager gnome-keyring wpasupplicant
+sudo apt install -y gdm3 network-manager gnome-keyring wpasupplicant
+sudo apt install -y gnome-terminal --no-install-suggests --no-install-recommends
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
