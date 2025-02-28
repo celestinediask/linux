bfaeda749e40e9bb0a7b9579c227432aa2da661f
etosy <152255839+etosy@users.noreply.github.com>
2025-02-28 22:25:09 +0000

    moved file

---
+++ arch_daily_driver.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# arch daily driver
+
+set -e
+
+start_time=$(date +%s)
+
+cp bashrc ~/.bashrc
+
+sudo pacman -Syu
+sudo pacman -S --needed --noconfirm - < arch/packages.txt
+
+# gnome setup
+sudo pacman -S --needed --noconfirm - < gnome/core_packages.txt
+sudo pacman -S --needed --noconfirm - < gnome/essential_packages.txt
+
+# gsettings
+bash gnome/gsettings_host.sh
+
+sudo systemctl enable gdm
+
+# setup yay
+
+# setup bluetooth
+sudo pacman -S bluez bluez-utils
+sudo systemctl start bluetooth
+sudo systemctl enable bluetooth
+
+# setup browser
+sudo pacman -S --noconfirm firefox
+# setup firefox profile
+sudo pacman -S --noconfirm firefox-ublock-origin
+sudo pacman -S --noconfirm firefox-dark-reader
+
+sudo pacman -S --noconfirm chromium
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+echo "successfully configured arch as daily driver in $execution_time seconds."
