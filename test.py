6b0d7bac48ed15b4a81f358761e3fa3c147a06ce
etosy <152255839+etosy@users.noreply.github.com>
2025-03-02 11:41:39 +0000

    test file backup

--- test.py
+++ test.py
@@ -5,12 +5,13 @@ GREEN = '\033[92m'
 RED = '\033[91m'
 RESET = '\033[0m'
 
-sources_list_file = '/etc/apt/sources.list'
+sources_list_file = '/etc/apt/sources.list.bak'
 
-def test_file_exists(file_path):
+def file_exists(file_path):
     return os.path.isfile(file_path)
 
-if test_file_exists(sources_list_file):
-    print(f"Test {GREEN}passed{RESET}: The file was found.")
+# test backup file sources_list_file
+if file_exists(sources_list_file):
+    print(f"Test {GREEN}passed{RESET}: backup file: {sources_list_file}.")
 else:
-    print(f"Test {RED}failed{RESET}: The file was not found.")
+    print(f"Test {RED}failed{RESET}: backup file: {sources_list_file}.")
