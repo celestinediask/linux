6dce59d899c33306d1dd62fbceeb74a3131547e1
etosy <152255839+etosy@users.noreply.github.com>
2025-03-02 06:57:26 +0530

    Update install_core_packages.sh

--- debian/install_core_packages.sh
+++ debian/install_core_packages.sh
@@ -4,9 +4,17 @@ set -e
 
 start_time=$(date +%s)
 
+PACKAGE_LIST="../gnome/core_packages.txt"
+
+# Check if the package list file exists
+if [ ! -f "$PACKAGE_LIST" ]; then
+  echo "Error: $PACKAGE_LIST not found!"
+  exit 1
+fi
+
 sudo apt update
 
-sudo apt install -y --no-install-suggests --no-install-recommends gnome-session gdm3 gnome-console network-manager gnome-keyring wpasupplicant
+sudo apt install -y --no-install-suggests --no-install-recommends $(cat "PACKAGE_LIST")
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
